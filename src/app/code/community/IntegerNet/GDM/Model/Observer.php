034b7bf678edb3fefacfbf33b1c41e6c06a9cd81
Andreas von Studnitz <avs@integer-net.de>
2012-12-05 17:47:27 +0100

    Adjusted database config form

--- src/app/code/community/IntegerNet/GDM/Model/Observer.php
+++ src/app/code/community/IntegerNet/GDM/Model/Observer.php
@@ -1,7 +1,22 @@
 <?php
 class IntegerNet_GDM_Model_Observer
 {
+    /**
+     * Set theme to "gdm" with fallback to "default"
+     *
+     * @param Varien_Event_Observer $observer
+     */
     public function predispatchInstallWizard($observer)
+    {
+        Mage::getDesign()->setTheme('gdm');
+    }
+
+    /**
+     * Set default Timezone and Currency
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function predispatchInstallStart($observer)
     {
         Mage::getSingleton('install/session')->setTimezone('Europe/Berlin');
         Mage::getSingleton('install/session')->setCurrency('EUR');
