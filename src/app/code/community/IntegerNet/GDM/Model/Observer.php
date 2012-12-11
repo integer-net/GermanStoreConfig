2eeb90b7778a0502f658d3675fed512102aa48a1
Andreas von Studnitz <avs@integer-net.de>
2012-12-11 12:11:00 +0100

    Hide other shipping methods if free shipping is possible, plus renaming of shipping methods

--- src/app/code/community/IntegerNet/GDM/Model/Observer.php
+++ src/app/code/community/IntegerNet/GDM/Model/Observer.php
@@ -21,4 +21,43 @@ class IntegerNet_GDM_Model_Observer
         Mage::getSingleton('install/session')->setTimezone('Europe/Berlin');
         Mage::getSingleton('install/session')->setCurrency('EUR');
     }
+
+    /**
+     * If Freeshipping method is allowed, disallow all other shipping methods
+     *
+     * @param Varien_Event_Observer $observer
+     * @return void
+     */
+    public function afterLoadCollection($observer)
+    {
+        $collection = $observer->getCollection();
+
+        if (!$collection instanceof Mage_Sales_Model_Resource_Quote_Address_Rate_Collection) return;
+
+        if (!$this->_isFreeshippingIncluded($collection)) return;
+
+        $shippingAddress = Mage::getSingleton('checkout/session')
+            ->getQuote()
+            ->getShippingAddress();
+
+        $shippingAddress->setLimitCarrier('freeshipping');
+    }
+
+    /**
+     * Checks if freeshipping method is included in collection for available shipping methods
+     *
+     * @param Mage_Sales_Model_Resource_Quote_Address_Rate_Collection $rateCollection
+     * @return boolean
+     */
+    protected function _isFreeshippingIncluded($rateCollection)
+    {
+        foreach ($rateCollection as $rate) {
+
+            /** @var $rate Mage_Sales_Model_Quote_Address_Rate */
+            if ($rate->getMethod() == 'freeshipping') {
+                return true;
+            }
+        }
+        return false;
+    }
 }
