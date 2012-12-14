9e572d409edb84d8854f4ac7a91b0b236526dc93
Andreas von Studnitz <avs@integer-net.de>
2012-12-14 11:36:57 +0100

    Added logo to Admin Header

--- src/app/code/community/IntegerNet/GermanStoreConfig/Model/Observer.php
+++ src/app/code/community/IntegerNet/GermanStoreConfig/Model/Observer.php
@@ -60,4 +60,49 @@ class IntegerNet_GermanStoreConfig_Model_Observer
         }
         return false;
     }
+
+    /**
+     * Adds GermanStoreConfig Logo to adminhtml header
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function afterAdminhtmlBlockHtml($observer)
+    {
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Adminhtml_Block_Page_Header && Mage::getStoreConfigFlag('admin/germanstoreconfig/display_logo')) {
+
+            $transport = $observer->getTransport();
+            $html = $transport->getHtml();
+            $divider = '<div class="header-right">';
+            $dividerPos = strpos($html, $divider);
+            $htmlBeforeDivider = substr($html, 0, $dividerPos);
+            $htmlAfterDivider = substr($html, $dividerPos);
+
+            $linkUrl = '';
+            $logoUrl = $this->_getLogoUrl($block);
+            $logoAlt = Mage::helper('germanstoreconfig')->__('German Store Configuration for Magento CE');
+
+            $newHtml = '<a href="' . $linkUrl . '" target="_blank">';
+            $newHtml .= '<img class="logo" src="' . $logoUrl . '" alt="' . $logoAlt . '';
+            $newHtml .= '</a>';
+
+            $transport->setHtml($htmlBeforeDivider . $newHtml . $htmlAfterDivider);
+        }
+    }
+
+    /**
+     * Get language dependant URL of germanstoreconfig logo
+     *
+     * @param Mage_Adminhtml_Block_Page_Header $block
+     * @return string
+     */
+    protected function _getLogoUrl($block)
+    {
+        $localeCode = Mage::app()->getLocale()->getLocaleCode();
+        if (strpos($localeCode, 'de_') === 0) {
+            return $block->getSkinUrl('images/logo-germanstoreconfig-de.gif');
+        } else {
+            return $block->getSkinUrl('images/logo-germanstoreconfig-en.gif');
+        }
+    }
 }
