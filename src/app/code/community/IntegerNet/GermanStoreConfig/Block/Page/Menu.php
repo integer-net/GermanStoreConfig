c4551351c8cf0c89069d003959fd2dec13c1311f
Andreas von Studnitz <avs@integer-net.de>
2013-02-19 11:10:28 +0100

    Made adminhtml menu configurable "ifconfig" and "class" nodes

---
+++ src/app/code/community/IntegerNet/GermanStoreConfig/Block/Page/Menu.php
@@ -0,0 +1,112 @@
+<?php
+class IntegerNet_GermanStoreConfig_Block_Page_Menu extends Mage_Adminhtml_Block_Page_Menu
+{
+    /**
+     * Recursive Build Menu array
+     * Added handling of "ifconfig" and "class" nodes
+     *
+     * @param Varien_Simplexml_Element $parent
+     * @param string $path
+     * @param int $level
+     * @return array
+     */
+    protected function _buildMenuArray(Varien_Simplexml_Element $parent=null, $path='', $level=0)
+    {
+        if (is_null($parent)) {
+            $parent = Mage::getSingleton('admin/config')->getAdminhtmlConfig()->getNode('menu');
+        }
+
+        $parentArr = array();
+        $sortOrder = 0;
+        foreach ($parent->children() as $childName => $child) {
+            if (1 == $child->disabled) {
+                continue;
+            }
+
+            $aclResource = 'admin/' . ($child->resource ? (string)$child->resource : $path . $childName);
+            if (!$this->_checkAcl($aclResource)) {
+                continue;
+            }
+
+            if ($child->depends && !$this->_checkDepends($child->depends)) {
+                continue;
+            }
+
+            if ($child->ifconfig && !Mage::getStoreConfigFlag((string)$child->ifconfig)) {
+                continue;
+            }
+
+            $menuArr = array();
+
+            $menuArr['label'] = $this->_getHelperValue($child);
+
+            $menuArr['sort_order'] = $child->sort_order ? (int)$child->sort_order : $sortOrder;
+
+            $menuArr['class'] = $child->class ? (string)$child->class : '';
+
+            if ($child->action) {
+                $menuArr['url'] = $this->_url->getUrl((string)$child->action, array('_cache_secret_key' => true));
+            } else {
+                $menuArr['url'] = '#';
+                $menuArr['click'] = 'return false';
+            }
+
+            $menuArr['active'] = ($this->getActive()==$path.$childName)
+                || (strpos($this->getActive(), $path.$childName.'/')===0);
+
+            $menuArr['level'] = $level;
+
+            if ($child->children) {
+                $menuArr['children'] = $this->_buildMenuArray($child->children, $path.$childName.'/', $level+1);
+            }
+            $parentArr[$childName] = $menuArr;
+
+            $sortOrder++;
+        }
+
+        uasort($parentArr, array($this, '_sortMenu'));
+
+        while (list($key, $value) = each($parentArr)) {
+            $last = $key;
+        }
+        if (isset($last)) {
+            $parentArr[$last]['last'] = true;
+        }
+
+        return $parentArr;
+    }
+
+    /**
+     * Get menu level HTML code
+     * Added display of "class" attribute
+     *
+     * @param array $menu
+     * @param int $level
+     * @return string
+     */
+    public function getMenuLevel($menu, $level = 0)
+    {
+        $html = '<ul ' . (!$level ? 'id="nav"' : '') . '>' . PHP_EOL;
+        foreach ($menu as $item) {
+            $html .= '<li ' . (!empty($item['children']) ? 'onmouseover="Element.addClassName(this,\'over\')" '
+                . 'onmouseout="Element.removeClassName(this,\'over\')"' : '') . ' class="'
+                . (!$level && !empty($item['active']) ? ' active' : '') . ' '
+                . (!empty($item['class']) ? ' ' . $item['class'] : '') . ' '
+                . (!empty($item['children']) ? ' parent' : '')
+                . (!empty($level) && !empty($item['last']) ? ' last' : '')
+                . ' level' . $level . '"> <a href="' . $item['url'] . '" '
+                . (!empty($item['title']) ? 'title="' . $item['title'] . '"' : '') . ' '
+                . (!empty($item['click']) ? 'onclick="' . $item['click'] . '"' : '') . ' class="'
+                . ($level === 0 && !empty($item['active']) ? 'active' : '') . '"><span>'
+                . $this->escapeHtml($item['label']) . '</span></a>' . PHP_EOL;
+
+            if (!empty($item['children'])) {
+                $html .= $this->getMenuLevel($item['children'], $level + 1);
+            }
+            $html .= '</li>' . PHP_EOL;
+        }
+        $html .= '</ul>' . PHP_EOL;
+
+        return $html;
+    }
+}
