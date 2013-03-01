84926a9fd84dc5feb26d9539094181d0294000d9
Andreas von Studnitz <avs@integer-net.de>
2013-03-01 13:15:50 +0100

    Display error message when news feed is not available.

--- src/app/code/community/IntegerNet/GermanStoreConfig/Block/News.php
+++ src/app/code/community/IntegerNet/GermanStoreConfig/Block/News.php
@@ -9,24 +9,28 @@ class IntegerNet_GermanStoreConfig_Block_News extends Mage_Adminhtml_Block_Templ
     {
         $feedUrl = Mage::getStoreConfig('germanstoreconfig/newsfeed_url');
 
-        $httpClient = new Zend_Http_Client($feedUrl);
+        try {
+            $httpClient = new Zend_Http_Client($feedUrl);
 
-        $response = $httpClient->request();
+            $response = $httpClient->request();
 
-        $news = new Varien_Simplexml_Element($response->getBody());
+            $news = new Varien_Simplexml_Element($response->getBody());
 
-        foreach ($news->channel->item as $item) {
-            $date = new Zend_Date((string)$item->pubDate, Zend_Date::RFC_822);
-            $items[$date->get(Zend_Date::TIMESTAMP)] = new Varien_Object(array(
-                'title' => (string)$item->title,
-                'description' => (string)$item->description,
-                'date' => $date->get(Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_MEDIUM)),
-                'link' => (string)$item->link,
-            ));
+            foreach ($news->channel->item as $item) {
+                $date = new Zend_Date((string)$item->pubDate, Zend_Date::RFC_822);
+                $items[$date->get(Zend_Date::TIMESTAMP)] = new Varien_Object(array(
+                    'title' => (string)$item->title,
+                    'description' => (string)$item->description,
+                    'date' => $date->get(Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_MEDIUM)),
+                    'link' => (string)$item->link,
+                ));
+            }
+
+            krsort($items);
+
+            return $items;
+        } catch (Exception $e) {
+            return array();
         }
-
-        krsort($items);
-
-        return $items;
     }
 }
